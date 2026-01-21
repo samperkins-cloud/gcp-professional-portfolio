45d0a82c6bf9fc29a1480e78f0a8e9e99e3d1fe5
Samuel Perkins <samuel.perkins@hackensackmeridian.org>
2026-01-20 19:49:57 -0500

    trigger build

--- projects/06-automated-container-deployment/apps/main.py
+++ projects/06-automated-container-deployment/apps/main.py
@@ -7,7 +7,7 @@ python
     @app.route("/")
     def hello_world():
         name = os.environ.get("NAME", "World")
-        return f"<h1>Hello, automated {name}!</h1><p>This is a containerized application deployed automatically by Cloud Build.</p>"
+        return f"<h1>Hello, {name}!</h1><p>This is a containerized application deployed automatically by Cloud Build.</p>"
 
     if __name__ == "__main__":
         app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
