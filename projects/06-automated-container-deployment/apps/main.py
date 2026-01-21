e1c016a79cbbefd0c404680eef6d204763ef81c3
Samuel Perkins <samuel.perkins@hackensackmeridian.org>
2026-01-20 20:59:05 -0500

    test cicd pipeline

--- projects/06-automated-container-deployment/apps/main.py
+++ projects/06-automated-container-deployment/apps/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return f"<h1>Hello, {name}!</h1><p>This is a containerized application deployed automatically by Cloud Build.</p>"
+    return f"<h1>Hello, {name}!</h1><p>This is a containerized application deployed automatically by Cloud Build. Build by the Google Associate Cloud Engineer, Sam Perkins.</p>"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
