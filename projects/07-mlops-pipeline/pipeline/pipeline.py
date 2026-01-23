9f94f6a2e4d35d3956e06664c6386653ae58503e
Samuel Perkins <samuel.perkins@hackensackmeridian.org>
2026-01-23 16:51:33 -0500

    feat(mlops): Add KFP pipeline definition script

---
+++ projects/07-mlops-pipeline/pipeline/pipeline.py
@@ -0,0 +1,41 @@
+from kfp import dsl
+from kfp.v2 import compiler
+from kfp.v2.dsl import component
+from google_cloud_aiplatform.v1beta1.schema.trainingjob.definition import \
+    custom_task
+
+# You can also use the ':latest' tag, but digests are better for production.
+# To find your digest, you can look at the final line of the Cloud Build output
+# or browse Artifact Registry in the Google Cloud Console.
+# For now, we will use the one from your output.
+TRAIN_COMPONENT_IMAGE_URI = "us-central1-docker.pkg.dev/my-portfolio-project-v2-484602/intelligent-api-components-repo/train-component@sha256:ca9a6c3e57d68dbf3374a2e7be13a8dd5bb760ebbc61275b59a1752b8c805c64"
+
+
+@component(
+    base_image=TRAIN_COMPONENT_IMAGE_URI,
+)
+def train_model():
+    """
+    This is a simple, one-step component that "trains" a model.
+    In reality, it just runs the main.py from our `components/train` directory.
+    The @component decorator wraps this function in the container image we specify.
+    """
+    # The container's ENTRYPOINT is ["python", "main.py"], so this component
+    # will execute that script when it runs. No additional code is needed here.
+    pass
+
+
+@dsl.pipeline(
+    name="intelligent-api-hello-world-pipeline",
+    description="A simple pipeline to test component execution."
+)
+def pipeline():
+    """Defines the structure of the pipeline."""
+    train_model_task = train_model()
+
+
+if __name__ == "__main__":
+    compiler.Compiler().compile(
+        pipeline_func=pipeline,
+        package_path="intelligent_api_pipeline.json"
+    )
