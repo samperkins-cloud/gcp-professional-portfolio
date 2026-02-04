e2e9b33c7d5e415972582ea014a6565fda137b6d
Samuel Perkins <samuel.perkins@hackensackmeridian.org>
2026-02-04 16:47:55 -0500

    final final test

--- projects/06-automated-container-deployment/apps/main.py
+++ projects/06-automated-container-deployment/apps/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return f"<h1>Hello, {name}!</h1><p>PULL REQUEST PREVIEW v3: This change is from a PR and is deployed to a temporary URL!</p>"
+    return f"<h1>Hello, {name}!</h1><p>PULL REQUEST PREVIEW v4: This change is from a PR and is deployed to a temporary URL!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
