ac9d7d9abc812c8f7af3eacafff16d6cab1ac5a0
Samuel Perkins <samuel.perkins@hackensackmeridian.org>
2026-02-08 11:36:30 -0500

    fix: use correct secret_id attribute for secret injection

--- projects/08-automated-functions-deployment/main.tf
+++ projects/08-automated-functions-deployment/main.tf
@@ -21,5 +21,5 @@ module "cicd_pipeline" {
   connection_name   = var.connection_name
   connection_region = var.location
   depends_on        = [module.project_apis]
-  secret_id = google_secret_manager_secret.app_secret.id
+  secret_id = google_secret_manager_secret.app_secret.secret_id
 }
