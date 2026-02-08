9493f242278f60a147125288c99bedf5d4ef8de6
Samuel Perkins <samuel.perkins@hackensackmeridian.org>
2026-02-08 12:25:06 -0500

    trigger pipeline

--- projects/08-automated-functions-deployment/apps/main.py
+++ projects/08-automated-functions-deployment/apps/main.py
@@ -12,4 +12,4 @@ def hello_etl(request):
     name = 'World'
     
     # This line displays the secret
-    return f"Hello, {escape(name)}! The first 5 chars of the API key are: {API_KEY[:5]}"
+    return f"Hello, {escape(name)}! The first 5 characters of the API key are: {API_KEY[:5]}"
