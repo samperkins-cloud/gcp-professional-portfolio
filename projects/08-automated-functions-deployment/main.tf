e07c5cee38b46ba3d98a4f83e7740e7d6383db99
Samuel Perkins <samuel.perkins@hackensackmeridian.org>
2026-02-28 15:20:14 -0500

    fix: Code for scheduler

--- projects/08-automated-functions-deployment/main.tf
+++ projects/08-automated-functions-deployment/main.tf
@@ -20,23 +20,3 @@ module "project_apis" {
   project_id = var.project_id
   api_list   = var.api_list
 }
-
-# --- 2. Automated CI/CD Deployment Pipeline for Cloud Functions ---
-module "cicd_pipeline" {
-  source                        = "../../modules/cloud-build-cicd-functions" 
-  project_id                    = var.project_id
-  location                      = var.location
-  app_name                      = var.app_name
-  github_owner                  = var.github_owner
-  github_repo_name              = var.github_repo_name
-  app_source_path               = var.app_source_path
-  connection_name               = var.connection_name
-  connection_region             = var.location
-  
-  # --- Values consumed from the Platform Workspace ---
-  secret_id                     = data.terraform_remote_state.platform.outputs.app_secret_id
-  runtime_service_account_email = data.terraform_remote_state.platform.outputs.app_service_account_email
-  pipeline_service_account_id   = data.terraform_remote_state.platform.outputs.pipeline_service_account_id # <-- ADD THIS LINE
-
-  depends_on                    = [module.project_apis]
-}
